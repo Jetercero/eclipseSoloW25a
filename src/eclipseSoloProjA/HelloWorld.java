ebdcd24ad800c2b6230221a5dd32d2505de05e63
jetercero <jtercero@emich.edu>
2026-02-14 13:31:44 -0500

    nifty new stuff updated

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github!");
-		System.out.println("Testing");
+		System.out.println("Testing with new update");
 	}
 
 }
